17b481b016a5b168d9187ec888aaee380004b355
Awili Uzo <uzo.awili@andela.com>
2016-09-16 11:56:22 -0500

    add home view

--- bookey/settings.py
+++ bookey/settings.py
@@ -38,14 +38,14 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_nose',
-    'books'
+    'books',
 )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middlewarpye.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
--- books/views.py
+++ books/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import Category, Book
 # Create your views here.
+
+class HomeView(View):
+    """
+    XXX
+    """
+
+    def get(self, request, *args, **kwargs):
+        """handles get requests to the home route"""
+        # url_params = request.GET
+        categories = Category.objects.all()
+        books = Book.objects.all()
+        context = {
+            'categories': categories,
+            'books': books
+        }
+        return render(request, 'books/home.html', context)
+
+
+
+
