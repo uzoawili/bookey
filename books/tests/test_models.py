9f9ff2d446bafa7fc1a40d1b88b0575dde79a4da
Awili Uzo <uzo.awili@andela.com>
2016-09-16 09:56:39 -0500

    add model tests

---
+++ books/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from ..models import Book, Category
+# Create your tests here.
+
+class CategoryTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_create_category(self):
+        pass
